f117269d4bee1f857eacaffe18328df393fec684
Gabriel Sousa Costa <costagsousa@gmail.com>
2026-08-17 17:36:29 -0300

    change gtag

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -8,6 +8,8 @@ export const QUOTE_REQUEST_URL = "https://wa.me/5562996001595";
 
 export function trackQuoteRequestConversion() {
   window.gtag?.("event", "conversion", {
-    send_to: "AW-18373262215",
+    send_to: "AW-18373262215/Aq7ZCCHwONwcEIf3hrlE",
+    value: 1.0,
+    currency: "BRL",
   });
 }
